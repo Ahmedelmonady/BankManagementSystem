3f2ad81d495ba5f0ed8dc286ddc4de4579c34f7f
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-11-16 15:32:47 +0200

    Defined the required constant variables

--- BankManagementSystem/utils.h
+++ BankManagementSystem/utils.h
@@ -6,5 +6,12 @@
 #include "string.h"
 #include "stdlib.h"
 
+#define DATABASE "BankDb.txt"
+#define COUNT "Count.txt"
+#define ADD_USER 1
+#define REMOVE_USER -1
+#define MAX_CHARS 25
+#define JSON_OUT ""
+#define JSON_IN ""
 
 #endif
